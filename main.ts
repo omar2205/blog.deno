21a9930df1a79efe6868251d77e908296565a51c
omar2205 <omar2205a@yahoo.com>
2022-07-02 05:55:06 +0200

    init blog and first post

---
+++ main.ts
@@ -0,0 +1,10 @@
+import blog from 'https://deno.land/x/blog@0.3.3/blog.tsx'
+
+
+blog({
+  title: 'Demo Blog',
+  author: 'oskr.nl',
+  links: [
+    { title: 'Email', url: 'hi@oskr.nl' }
+  ]
+})
---
+++ posts/hello_blog.md
@@ -0,0 +1,11 @@
+---
+title: Hello Blog!
+publish_date: 2022-07-02
+background: white
+---
+
+### Hello Blog!
+
+This is my first post.
+
+
